5db5d9fa655574d11d790b74c6119b3db20c4e6c
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-10 13:12:51 +0300

    add struture

---
+++ src/components/TaskImageFinder/TaskImageFinder.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import s from './TaskImageFinder.module.css';
+
+export default class TaskImageFinder extends Component {
+  state = {};
+
+  render() {
+    return <div className={s.box}>TaskImageFinder</div>;
+  }
+}
